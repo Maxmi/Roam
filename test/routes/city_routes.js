43fbfcb402365ce3c7824de5fffa7594252ed2b2
Gulmira Berdalieva <gulmira2410@gmail.com>
2018-05-01 16:27:05 -0700

    tests for city routes

---
+++ test/routes/city_routes.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = require('chai');
+const { resetDb } = require('../db/helpers');
+const app = require('../../src/app');
+
+chai.use(chaiHttp);
+
+describe('/GET /cities/:city', () => {
+  before(() => {
+    return resetDb();
+  });
+  it('should return a city page', () => {
+    return chai.request(app)
+      .get('/cities/testcity')
+      .then(res => {
+        expect(res).to.be.html;
+        expect(res).to.have.status(200);
+      });
+  });
+});
